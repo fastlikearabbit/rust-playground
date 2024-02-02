4f74e2e16e4d1b46afebe977d4e2fbae3c2fe7e9
Daniel Ghenghea <daniel.ghenghea2000@gmail.com>
2024-02-02 16:09:24 +0200

    implemented min-queue

--- intro/min-queue/src/lib.rs
+++ intro/min-queue/src/lib.rs
@@ -1,45 +1,79 @@
 #![forbid(unsafe_code)]
 
 use std::collections::VecDeque;
+use std::cmp::min;
 
 #[derive(Default)]
 pub struct MinQueue<T> {
-    // TODO: your code goes here.
+    in_stack : VecDeque<(T, T)>,
+    out_stack: VecDeque<(T, T)>,
+    size: usize,
 }
 
 impl<T: Clone + Ord> MinQueue<T> {
     pub fn new() -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        Self {
+            in_stack : VecDeque::new(),
+            out_stack: VecDeque::new(),
+            size: 0,
+        }
     }
 
     pub fn push(&mut self, val: T) {
-        // TODO: your code goes here.
-        unimplemented!()
+        let mut minimum = &val;
+        if let Some((_, in_min)) = self.in_stack.front() {
+            minimum = min(minimum, in_min);
+        }
+        let minimum = minimum.clone();
+        self.in_stack.push_front((val, minimum));
+        self.size += 1;
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        // TODO: your code goes here.
-        unimplemented!()
+        if self.out_stack.is_empty() {
+            while let Some((in_top, _)) = self.in_stack.pop_front() {
+                let mut minimum = &in_top;
+                if let Some((_, out_min)) = self.out_stack.front() {
+                    minimum = min(&in_top, out_min);
+                }
+                let minimum = minimum.clone();
+                self.out_stack.push_front((in_top, minimum));
+            }
+        }
+        if let Some((top, _)) = self.out_stack.pop_front() {
+            self.size -= 1;
+            Some(top)
+        } else {
+            None
+        }
     }
 
     pub fn front(&self) -> Option<&T> {
-        // TODO: your code goes here.
-        unimplemented!()
+        match self.out_stack.front() {
+            Some((top, _)) => Some(top),
+            _              => if let Some((in_stack_back, _)) = self.in_stack.back() {
+                                Some(in_stack_back)
+                            } else {
+                                None
+                            }
+        }
     }
 
     pub fn min(&self) -> Option<&T> {
-        // TODO: your code goes here.
-        unimplemented!()
+        match (self.in_stack.front(), self.out_stack.front()) {
+            (Some((_, in_min)), Some((_, out_min))) => Some(min(in_min, out_min)),
+            (Some((_, in_min)), _) => Some(in_min),
+            (_, Some((_, out_min))) => Some(out_min),
+            _ => None
+        }
+
     }
 
     pub fn len(&self) -> usize {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.size
     }
 
     pub fn is_empty(&self) -> bool {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.size == 0
     }
 }
