dd56078ff670b85aa3bbbf83765cb61d5c1809ff
Daniel Ghenghea <daniel.ghenghea2000@gmail.com>
2024-02-05 21:52:21 +0200

    implementaed structures and basic methods

--- borrowing/pstack/src/lib.rs
+++ borrowing/pstack/src/lib.rs
@@ -1,29 +1,40 @@
 #![forbid(unsafe_code)]
-use std::rc::Rc;
+use std::{path::Iter, rc::Rc};
 
 pub struct PRef<T> {
-    // TODO: your code goes here.
+    data: T,
+    prev: Rc<PRef<T>>,
 }
 
 impl<T> std::ops::Deref for PRef<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        // TODO: your code goes here.
-        unimplemented!()
+        &self.data
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 pub struct PStack<T> {
-    // TODO: your code goes here.
+    top : Option<PRef<T>>,
+    size: usize,
 }
 
 impl<T> Default for PStack<T> {
     fn default() -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        Self {
+            top : None,
+            size: 0,
+        }
+    }
+}
+
+impl<T> Iterator for PStack<T> {
+    type Item = PRef<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!();
     }
 }
 
@@ -36,8 +47,7 @@ impl<T> Clone for PStack<T> {
 
 impl<T> PStack<T> {
     pub fn new() -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        Default::default()
     }
 
     pub fn push(&self, value: T) -> Self {
@@ -51,18 +61,15 @@ impl<T> PStack<T> {
     }
 
     pub fn len(&self) -> usize {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.size
     }
 
     pub fn is_empty(&self) -> bool {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.size == 0
     }
 
     pub fn iter(&self) -> impl Iterator<Item = PRef<T>> {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.clone()
     }
 }
 
