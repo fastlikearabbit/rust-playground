4524b9c2cec9accaf7f6ac8815e6a25e23a3103d
Daniel Ghenghea <daniel.ghenghea2000@gmail.com>
2024-02-22 17:27:12 +0200

    optimize

--- std-collections/prefix/src/lib.rs
+++ std-collections/prefix/src/lib.rs
@@ -1,21 +1,20 @@
 #![forbid(unsafe_code)]
 
-pub fn longest_common_prefix(mut strs: Vec<&str>) -> String {
+pub fn longest_common_prefix(strs: Vec<&str>) -> String {
     if strs.len() == 0 { return String::new(); }
+    let first_str = strs[0];
+    if strs.len() == 1 { return first_str.to_string(); }
 
-    let last_string_chars = strs.pop().unwrap().chars();
-    let mut result: Vec<char> = vec![];
-
-    let char_iters: Vec<_>= strs.into_iter().map(|str| str.chars()).collect();
-    
-    // for in_char in last_string_chars {
-    //     for char_iter in &char_iters {
-    //         let out_char: Option<char> = char_iter.next();
-    //         if out_char.is_none() || in_char != out_char.unwrap() { return result.into_iter().collect(); }
-    //         let out_char = out_char.unwrap();
-    //     }
-    //     result.push(in_char);
-    // }
-
-    result.into_iter().collect()
+    let mut result = String::new();
+    let vec_chars: Vec<_>= strs[1..].into_iter().map(|str| str.chars().collect::<Vec<_>>()).collect();
+   
+    for (i, char) in first_str.char_indices() {
+        for str in &vec_chars {
+            if i >= str.len() || char != str[i] {
+                return result;
+            }
+        }
+        result.push(char);
+    }
+    result
 }
