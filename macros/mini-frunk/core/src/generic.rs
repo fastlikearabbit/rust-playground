78323617344c26a7763a95b3f039b2118cbd0bdb
Daniel Ghenghea <daniel.ghenghea2000@gmail.com>
2024-03-03 15:39:53 +0200

    create method signatures

--- macros/mini-frunk/core/src/generic.rs
+++ macros/mini-frunk/core/src/generic.rs
@@ -6,8 +6,24 @@ pub trait Generic {
     fn from(repr: Self::Repr) -> Self;
 }
 
-// fn generic<Dst, Repr>(repr: Repr) -> Dst { ... }
-// fn into_generic<Src, Repr>(src: Src) -> Repr { ... }
-// fn convert_from<Src, Dst, Repr>(src: Src) -> Dst { ... }
+fn generic<Dst, Repr>(repr: Repr) -> Dst 
+where
+    Dst: Generic<Repr = Repr>,
+{ 
+    todo!()
+}
+fn into_generic<Src, Repr>(src: Src) -> Repr
+where
+    Src: Generic<Repr = Repr>
+{ 
+    todo!()
+}
+fn convert_from<Src, Dst, Repr>(src: Src) -> Dst
+where
+    Src: Generic<Repr = Repr>,
+    Dst: Generic<Repr = Repr>,
+{ 
+    todo!()
+}
 
 // TODO: your code goes here.
