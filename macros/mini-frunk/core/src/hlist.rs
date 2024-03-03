a7c4326cc316b6d80ce30e9e29974f3bfffaef1a
Daniel Ghenghea <daniel.ghenghea2000@gmail.com>
2024-03-03 23:05:26 +0200

    implement hlist related macros

--- macros/mini-frunk/core/src/hlist.rs
+++ macros/mini-frunk/core/src/hlist.rs
@@ -26,8 +26,39 @@ impl<H, T> HCons<H, T> {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// macro_rules! HList
-// macro_rules! hlist
-// macro_rules! hlist_pat
+#[macro_export]
+macro_rules! HList {
+    () => {
+        HNil::new()
+    };
 
-// TODO: your code goes here.
+    ($t:ty) => {
+        HCons<$t, HNil>
+    };
+
+    ($front: ty, $($rest:ty),+) => {
+        HCons<$front, HList!($($rest),+)>
+    };
+}
+
+#[macro_export]
+macro_rules! hlist {
+    () => {
+        HNil::new()
+    };
+
+    ($e:expr) => {
+        HCons::new($e, HNil)
+    };
+
+    ($front: expr, $($rest:expr),+) => {
+        HCons::new($front, hlist!($($rest),+))
+    };
+}
+
+#[macro_export]
+macro_rules! hlist_pat {
+    ($($i: ident),*) => {
+        ($($i),*)
+    };
+}
